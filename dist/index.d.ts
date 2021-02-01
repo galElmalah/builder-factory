a667dc6cf22e87633d9169735873cc14af778e13
Gal Elmalah <gale@wix.com>
2021-02-01 18:43:46 +0200

    update package name

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,16 @@
-export declare const createBuilder: () => void;
+interface Factory<T> {
+    aBuilder(): API<T>;
+}
+declare type API<T> = {
+    [K in keyof T as `with${Capitalize<string & K>}`]: (value: T[K]) => API<T> & BaseMethods<T>;
+} & BaseMethods<T>;
+declare type BaseMethods<T> = {
+    build: () => T;
+    getSchema: () => T;
+};
+/**
+ *
+ * @param schema An object containing the default object. The builder type will be inferred from the structure of the scheme object.
+ */
+export declare const builderFactory: <T>(schema: T) => Factory<T>;
+export {};
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,38 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createBuilder = void 0;
-const createBuilder = () => {
-    //my awesome module
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.createBuilder = createBuilder;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.builderFactory = void 0;
+const lodash_clonedeep_1 = __importDefault(require("lodash.clonedeep"));
+const capitalize = (word) => {
+    const [firstChar, ...restOfTheWord] = word;
+    return firstChar.toUpperCase() + restOfTheWord.join('');
+};
+/**
+ *
+ * @param schema An object containing the default object. The builder type will be inferred from the structure of the scheme object.
+ */
+const builderFactory = (schema) => {
+    const aBuilder = () => {
+        const targetObject = lodash_clonedeep_1.default(schema);
+        const api = {
+            build: () => {
+                return targetObject;
+            },
+            getSchema: () => {
+                return lodash_clonedeep_1.default(schema);
+            },
+        };
+        for (const key in schema) {
+            api[`with${capitalize(key)}`] = (value) => {
+                targetObject[key] = value;
+                return api;
+            };
+        }
+        return api;
+    };
+    return { aBuilder };
+};
+exports.builderFactory = builderFactory;
 //# sourceMappingURL=index.js.map
